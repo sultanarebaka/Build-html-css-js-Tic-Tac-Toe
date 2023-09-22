66dcd9c4f4322e4070b260f1c7e676c16b139101
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-09-22 22:08:40 +0600

    Update main.js Tic-Tac-Toe.js

--- main.js Tic-Tac-Toe.js
+++ main.js Tic-Tac-Toe.js
@@ -9,6 +9,30 @@
    
    
 const cells =document.querySelectorAll('.cell');
+
+const winCombos = [
+
+[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8],
+[0, 3, 6],
+[1, 4, 7],
+[2, 5, 8],
+[0, 4, 8],
+[6, 4, 2]
+
+]
+
+
+
+
+
+
+
+
+
+
+
    
    startGame();
 
